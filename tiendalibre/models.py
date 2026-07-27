b2d0bdc7a87c976f288abd5292b1d4dcee21de01
Nicolas Ledesma <nicoledesma25@gmail.com>
2026-07-27 09:04:19 -0300

    chore: estructura inicial del proyecto tiendalibre

---
+++ tiendalibre/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.IntegerField()
+    marca = models.CharField(max_length=50, default='Marca Desconocida')
+
+    def __str__(self):
+        return f'{self.nombre} - {self.marca} - ${self.precio} - Stock: {self.stock}'
